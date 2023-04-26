4016bc9d1f6f2369edff5d5b47f118d777cfb178
mahathi0412 <mandelli@pdx.edu>
2023-04-26 15:39:17 -0700

    completed ex-1, 2, 3

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,22 @@
-// Add your code here
+const input = document.getElementById("input");
+const textContainer = document.getElementById("text-container");
 
-input.addEventListener('keydown', handleKeyDown);
+input.addEventListener("input", () => {
+  const keyword = input.value;
+  const text = textContainer.textContent;
+  const words = text.split(" ");
+
+  if (keyword.trim() !== "") {
+    const highlightedText = words
+      .map((word) =>
+        word.toLowerCase().includes(keyword.toLowerCase())
+          ? `<span class="bg-warning">${word}</span>`
+          : word
+      )
+      .join(" ");
+
+    textContainer.innerHTML = highlightedText;
+  } else {
+    textContainer.innerHTML = text;
+  }
+});
